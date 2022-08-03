02b97704a63945fbeeaf10f5c215abdda125a148
BrandonC123 <brandoc318@gmail.com>
2022-08-03 10:23:30 -0700

    Refactor viewing repository list, accidentally coded it into view profile.

--- src/components/AuthenticatedRoutes.jsx
+++ src/components/AuthenticatedRoutes.jsx
@@ -7,6 +7,8 @@ import MainHeader from "./MainHeader";
 import { UserContext } from "./UserContext";
 import ViewRepository from "./ViewRepository";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+import ViewProfile from "./ViewProfile";
+import ViewRepositoryList from "./ViewRepositoryList";
 
 const AuthenticateRoutes = () => {
     const auth = getAuth();
@@ -24,6 +26,11 @@ const AuthenticateRoutes = () => {
                 <MainHeader username={user.displayName} />
                 <Routes>
                     <Route path="/" element={<Home />} />
+                    <Route path="/:username" element={<ViewProfile />} />
+                    <Route
+                        path="/:username/repositories"
+                        element={<ViewRepositoryList />}
+                    />
                     <Route path="/new" element={<CreateRepository />} />
                     <Route
                         path="/:username/:repoName"
--- src/components/ViewProfile.jsx
+++ src/components/ViewProfile.jsx
@@ -4,53 +4,10 @@ import { getStorage, ref, listAll } from "firebase/storage";
 import { UserContext } from "./UserContext";
 
 const ViewProfile = () => {
-    const user = useContext(UserContext);
-    const [repoList, setRepoList] = useState([]);
-    const storage = getStorage();
-    useEffect(() => {
-        const listRef = ref(storage, `/${user.uid}/repos`);
-        let tempList = [];
-        listAll(listRef)
-            .then((res) => {
-                if (res.prefixes.length !== repoList.length) {
-                    res.prefixes.forEach((folderRef) => {
-                        tempList.push(folderRef.name);
-                        console.log(folderRef);
-                    });
-                    setRepoList(tempList);
-                }
-            })
-            .catch((error) => {
-                console.error(error);
-            });
-    }, [user, storage]);
-    function displayRepoList() {
-        return repoList.map((repo) => {
-            return (
-                <div className="profile-repo-container border-divider row">
-                    <div className="profile-repo-info column">
-                        <Link
-                            to={`/${user.displayName}/${repo}`}
-                            className="profile-repo-link"
-                        >
-                            {repo}
-                        </Link>
-                        <span>Updated today</span>
-                    </div>
-                    <div className="profile-repo-actions"><button>Star</button></div>
-                </div>
-            );
-        });
-    }
+  
     return (
-        <div className="profile-page page">
-            <div className="profile-information-column">
-                <img src="/img/default-profile-pic.png" alt="" />
-                <h1></h1>
-                <h2>{user.displayName}</h2>
-                <button>Edit profile</button>
-            </div>
-            <main className="profile-repo-list">{displayRepoList()}</main>
+        <div className=" page">
+           
         </div>
     );
 };
