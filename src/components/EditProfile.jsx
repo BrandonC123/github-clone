3d38a7bc1090f5c44d9f3ae2b94a4e46ca6c0910
BrandonC123 <brandoc318@gmail.com>
2022-08-05 20:58:45 -0700

    Write misc user data to realtime database.

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -1,14 +1,52 @@
+import { getDatabase, ref, set } from "firebase/database";
+import { useContext, useState } from "react";
+import { UserContext } from "./UserContext";
+
 const EditProfile = ({ display, toggleForm }) => {
+    const [name, setName] = useState("");
+    const [bio, setBio] = useState("");
+    const [company, setCompany] = useState("");
+    const [location, setLocation] = useState("");
+    const [email, setEmail] = useState("");
+    const [website, setWebsite] = useState("");
+    const [twitterUsername, setTwitterUsername] = useState("");
+
+    const user = useContext(UserContext);
+    const db = getDatabase();
+    function writeUserData() {
+        set(ref(db, "users/" + user.uid), {
+            name: name,
+            bio: bio,
+            company: company,
+            location: location,
+            email: email,
+            website: website,
+            twitterUsername: twitterUsername,
+        });
+    }
+
     return (
         display && (
-            <form className="edit-profile-form">
+            <form className="edit-profile-form column">
                 <li className="edit-profile-input column">
                     <label htmlFor="full-name">Name</label>
-                    <input type="text" placeholder="Name" id="full-name" />
+                    <input
+                        onChange={(e) => {
+                            setName(e.target.value);
+                        }}
+                        value={name}
+                        type="text"
+                        placeholder="Name"
+                        id="full-name"
+                    />
                 </li>
                 <li className="edit-profile-input column">
                     <label htmlFor="profile-bio">Bio</label>
                     <textarea
+                        onChange={(e) => {
+                            setBio(e.target.value);
+                        }}
+                        value={bio}
                         name=""
                         cols="30"
                         rows="5"
@@ -17,32 +55,80 @@ const EditProfile = ({ display, toggleForm }) => {
                     ></textarea>
                 </li>
                 <li className="edit-profile-input">
-                    <input type="text" placeholder="Company" id="company" />
-                </li>
-                <li className="edit-profile-input">
-                    <input type="text" placeholder="Location" id="location" />
-                </li>
-                <li className="edit-profile-input">
-                    <input type="email" id="email" />
-                </li>
-                <li className="edit-profile-input">
-                    <input type="text" placeholder="Website" id="website" />
+                    <input
+                        onChange={(e) => {
+                            setCompany(e.target.value);
+                        }}
+                        value={company}
+                        type="text"
+                        placeholder="Company"
+                        id="company"
+                    />
                 </li>
                 <li className="edit-profile-input">
                     <input
+                        onChange={(e) => {
+                            setLocation(e.target.value);
+                        }}
+                        value={location}
+                        type="text"
+                        placeholder="Location"
+                        id="location"
+                    />
+                </li>
+                <li className="edit-profile-input">
+                    <input
+                        onChange={(e) => {
+                            setEmail(e.target.value);
+                        }}
+                        value={email}
+                        type="email"
+                        placeholder="Email"
+                        id="email"
+                    />
+                </li>
+                <li className="edit-profile-input">
+                    <input
+                        onChange={(e) => {
+                            setWebsite(e.target.value);
+                        }}
+                        value={website}
+                        type="url"
+                        placeholder="Website"
+                        id="website"
+                    />
+                </li>
+                <li className="edit-profile-input">
+                    <input
+                        onChange={(e) => {
+                            setTwitterUsername(e.target.value);
+                        }}
+                        value={twitterUsername}
                         type="text"
                         placeholder="Twitter username"
                         id="twitter-username"
                     />
                 </li>
-                <button
-                    onClick={() => {
-                        toggleForm();
-                    }}
-                >
-                    Save
-                </button>
-                <button>Cancel</button>
+                <div className="edit-profile-action">
+                    <button
+                        onClick={() => {
+                            writeUserData();
+                            toggleForm();
+                        }}
+                        type="button"
+                        className="green-action-btn btn"
+                    >
+                        Save
+                    </button>
+                    <button
+                        onClick={() => {
+                            toggleForm();
+                        }}
+                        type="button"
+                    >
+                        Cancel
+                    </button>
+                </div>
             </form>
         )
     );
