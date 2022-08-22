efaee94e1ec9b4e30e77016290f59000f132925e
BrandonC123 <brandoc318@gmail.com>
2022-08-21 22:55:44 -0700

    Implement code to display repositories from followed users within the past 10 days. Used date-fns to calculate difference in days.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,68 @@
+import { differenceInCalendarDays } from "date-fns";
+import { useState } from "react";
+import { useEffect } from "react";
 import { useContext } from "react";
+import RepositoryService from "../services/RepositoryService";
+import UserService from "../services/UserService";
 import Sidebar from "./Sidebar";
 import { UserContext } from "./UserContext";
 
 const Home = ({ loading }) => {
     const user = useContext(UserContext);
+    const [followList, setFollowList] = useState([]);
+    const [repoDisplayList, setDisplayRepoList] = useState([]);
 
+    useEffect(() => {
+        if (user) {
+            initializeFollowRepos();
+        }
+    }, [user]);
+    async function initializeFollowRepos() {
+        const currentDate = new Date();
+        // Get recent repositories of followed users
+        const serviceFollowList = await UserService.getFollowList(
+            user.displayName
+        );
+        setFollowList(serviceFollowList);
+        let tempDisplayList = [];
+        serviceFollowList.forEach(async function (username) {
+            const serviceRepoList = await RepositoryService.getRepoList(
+                username
+            );
+            serviceRepoList.forEach((repo) => {
+                if (
+                    differenceInCalendarDays(
+                        currentDate,
+                        new Date(repo.lastUpdated.seconds * 1000)
+                    ) <= 10
+                ) {
+                    tempDisplayList.push({ ...repo, username });
+                    setDisplayRepoList(tempDisplayList);
+                }
+            });
+        });
+    }
+    function displayFollowedRepos() {
+        return repoDisplayList.map((repo) => {
+            return (
+                <p>
+                    {repo.username} {repo.repoName}
+                </p>
+            );
+        });
+    }
     function displayHome() {
         if (user) {
             return (
                 <div className="home-page row">
                     <Sidebar />
                     <section className="home-page-section two-column">
-                        <div className="home-page-content">Following</div>
+                        <div className="home-page-content">
+                            Following
+                            <div className="followed-repos-container">
+                                {displayFollowedRepos()}
+                            </div>
+                        </div>
                         <div className="home-page-misc">
                             <div className="home-misc-text column">
                                 <h4>Adding web cookies for enterprise users</h4>
