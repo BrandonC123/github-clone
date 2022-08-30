a3b050cf0ffccc6938893c2c752b1d3a51c652fd
BrandonC123 <brandoc318@gmail.com>
2022-08-30 14:13:57 -0700

    Implement starring/unstarring repositories. Also started on searching for repositories.

--- src/components/ViewRepositoryList.jsx
+++ src/components/ViewRepositoryList.jsx
@@ -5,6 +5,8 @@ import ProfileInformation from "./ProfileInformation";
 import ProfileNav from "./ProfileNav";
 import RepositoryService from "../services/RepositoryService";
 import ViewRepositoryListDropdown from "./ViewRepositoryListDropdown";
+import star from "../img/star-icon.svg";
+import filledStar from "../img/filled-star-icon.svg";
 
 const ViewRepositoryList = () => {
     // TODO: Add functionality to star repos
@@ -16,6 +18,7 @@ const ViewRepositoryList = () => {
     const [lastUpdatedList, setLastUpdatedList] = useState([]);
     const [nameSortList, setNameSortList] = useState([]);
     const [starSortList, setStarSortList] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
 
     useEffect(() => {
         // Use username from Params to allow for viewing other user's repos
@@ -30,7 +33,15 @@ const ViewRepositoryList = () => {
                 setStarSortList(starRepoList);
             }
         });
-    }, [user]);
+    }, [username]);
+    useEffect(() => {
+        let tempRepoList = Array.from(repoList);
+        tempRepoList = tempRepoList.filter((repo) =>
+            repo.repoName.includes(searchTerm)
+        );
+        // console.log(tempRepoList);
+        setRepoList(tempRepoList);
+    }, [searchTerm]);
     function sortByLastUpdated(repoList) {
         let tempLastUpdated = Array.from(repoList);
         tempLastUpdated
@@ -50,15 +61,39 @@ const ViewRepositoryList = () => {
         setNameSortList(tempNameSort);
     }
     function toggleStarBtn(btn) {
-        let starIcon = btn.firstChild;
         let btnText = btn.lastChild.textContent;
         if (btnText === "Star") {
             btn.lastChild.textContent = "Starred";
-            // btn.firstChild.innerHtml =
+            btn.firstChild.src = filledStar;
         } else {
             btn.lastChild.textContent = "Star";
-            btn.firstChild.src = "/img/star-icon.svg";
+            btn.firstChild.src = star;
         }
+    }
+    function displayStarBtn(repo, starred) {
+        const starSrc = starred ? filledStar : star;
+        const btnText = starred ? "Starred" : "Star";
+        return (
+            <button
+                onClick={(e) => {
+                    RepositoryService.starRepo(
+                        user.displayName,
+                        repo.repoName,
+                        repoList
+                    );
+                    // Make sure target is the button not img/span
+                    const btn =
+                        e.target === e.currentNode
+                            ? e.target
+                            : e.target.parentNode;
+                    toggleStarBtn(btn);
+                }}
+                className="secondary-gray-btn btn vertical-center"
+            >
+                <img src={starSrc} alt="Star icon" />
+                <span>{btnText}</span>
+            </button>
+        );
     }
     // Update repo list to display current sort by selection
     function toggleRepoList(sortType) {
@@ -95,35 +130,7 @@ const ViewRepositoryList = () => {
                         </small>
                     </div>
                     <div className="profile-repo-actions">
-                        <button
-                            onClick={(e) => {
-                                // RepositoryService.starRepo(
-                                //     user.displayName,
-                                //     repo.repoName,
-                                //     repoList
-                                // );
-                                toggleStarBtn(e.target);
-                            }}
-                            className="secondary-gray-btn btn vertical-center"
-                        >
-                            <svg
-                                fill="#8b949e"
-                                xmlns="http://www.w3.org/2000/svg"
-                                aria-hidden="true"
-                                height="16"
-                                viewBox="0 0 16 16"
-                                version="1.1"
-                                width="16"
-                                data-view-component="true"
-                            >
-                                <path
-                                    fill-rule="evenodd"
-                                    d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z"
-                                ></path>
-                            </svg>
-
-                            <span>Star</span>
-                        </button>
+                        {displayStarBtn(repo, repo.starred)}
                     </div>
                 </div>
             );
@@ -135,7 +142,11 @@ const ViewRepositoryList = () => {
             <main className="profile-repo-list">
                 <ProfileNav username={username} />
                 <div className="row space-between align-center">
+                    {/* TODO: search bar */}
                     <input
+                        onChange={(e) => {
+                            setSearchTerm(e.target.value);
+                        }}
                         type="search"
                         placeholder="Find a repository..."
                         className="search-bar"
@@ -145,19 +156,21 @@ const ViewRepositoryList = () => {
                             toggleRepoList={toggleRepoList}
                         />
                     </div>
-                    <button
-                        onClick={() => {
-                            navigate("/new");
-                        }}
-                        className="green-action-btn btn vertical-center"
-                    >
-                        <img
-                            width={"15px"}
-                            src="/img/repo-icon.svg"
-                            alt="Repository icon"
-                        />
-                        New
-                    </button>
+                    {user.displayName === username && (
+                        <button
+                            onClick={() => {
+                                navigate("/new");
+                            }}
+                            className="green-action-btn btn vertical-center"
+                        >
+                            <img
+                                width={"15px"}
+                                src={require("../img/repo-icon.svg").default}
+                                alt="Repository icon"
+                            />
+                            New
+                        </button>
+                    )}
                 </div>
                 {displayRepoList()}
             </main>
