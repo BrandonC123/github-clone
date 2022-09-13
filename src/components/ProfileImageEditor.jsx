9ab6d3e7fd929799657ed1662b4a8c0551c18f1f
BrandonC123 <brandoc318@gmail.com>
2022-09-13 12:23:19 -0700

    Prototype of choosing user profile image and retrieving the resulting square cropped image with react-avatar-editor.

--- src/components/ProfileImageEditor.jsx
+++ src/components/ProfileImageEditor.jsx
@@ -1,20 +1,56 @@
+import React, { useState } from "react";
 import AvatarEditor from "react-avatar-editor";
+import ReactCrop from "react-image-crop";
+import "react-image-crop/dist/ReactCrop.css";
+import image from "../img/default-profile-pic.png";
+import test from "../img/test.jpeg";
 
-const ProfileImageEditor = ({ display }) => {
-    return (
-        display && (
-            <AvatarEditor
-                image="/img/lock-icon.svg"
-                width={250}
-                height={250}
-                border={10}
-                borderRadius={125}
-                color={[255, 255, 255, 0.6]} // RGBA
-                scale={1.2}
-                rotate={0}
-            />
-        )
-    );
-};
+class ProfileImageEditor extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            newImgSrc: "",
+        };
+    }
+
+    setEditorRef = (editor) => {
+        if (editor) {
+            this.editor = editor;
+            const canvasScaled = this.editor.getImageScaledToCanvas();
+            canvasScaled.toBlob((blob) => {
+                console.log(blob);
+                const url = URL.createObjectURL(blob);
+                console.log(url);
+                this.setState({
+                    newImgSrc: url,
+                });
+            });
+        }
+    };
+    render() {
+        const { src } = this.props;
+        return (
+            <>
+                <AvatarEditor
+                    ref={this.setEditorRef}
+                    image={src}
+                    width={250}
+                    height={250}
+                    border={20}
+                    borderRadius={125}
+                    color={[0, 0, 0, 0.6]} // RGBA
+                />
+                <img id="image" src={this.state.newImgSrc} alt="" />
+                <button
+                    onClick={() => {
+                        this.setEditorRef(this.editor);
+                    }}
+                >
+                    Upload
+                </button>
+            </>
+        );
+    }
+}
 
 export default ProfileImageEditor;
