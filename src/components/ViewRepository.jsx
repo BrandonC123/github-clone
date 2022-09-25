ad1115cbf61cfe621673c90e947ddc9f0eee7d45
BrandonC123 <brandoc318@gmail.com>
2022-09-24 23:25:08 -0700

    Polish styling and refactor displaying repository content for ViewRepo route.

--- src/components/ViewRepository.jsx
+++ src/components/ViewRepository.jsx
@@ -4,12 +4,16 @@ import { UserContext } from "./UserContext";
 import RepositoryNav from "./RepositoryNav";
 import RepositoryService from "../services/RepositoryService";
 import Dropdown from "./Dropdown";
+import UserService from "../services/UserService";
+// import folderIcon from "../img/folder-icon.svg";
+// import fileIcon from "../img/file-icon.svg";
 
 const ViewRepository = () => {
     // TODO: Make sure repository exists before displaying
     const user = useContext(UserContext);
     const { username } = useParams();
     const { repoName } = useParams();
+    const [profileImg, setProfileImg] = useState("");
     const [repoFolders, setRepoFolders] = useState([]);
     const [repoItems, setRepoItems] = useState([]);
 
@@ -23,52 +27,58 @@ const ViewRepository = () => {
                 setRepoItems(res.tempItemList);
             }
         });
-    }, [user]);
-    function displayFolders() {
-        return repoFolders.map((folder) => {
-            return (
-                <div key={folder} className="view-repo-folder">
-                    <svg
-                        fill="#8b949e"
-                        xmlns="http://www.w3.org/2000/svg"
-                        aria-label="Directory"
-                        aria-hidden="true"
-                        height="16"
-                        viewBox="0 0 16 16"
-                        version="1.1"
-                        width="16"
-                        data-view-component="true"
-                        className="margin-right"
-                    >
-                        <path d="M1.75 1A1.75 1.75 0 000 2.75v10.5C0 14.216.784 15 1.75 15h12.5A1.75 1.75 0 0016 13.25v-8.5A1.75 1.75 0 0014.25 3H7.5a.25.25 0 01-.2-.1l-.9-1.2C6.07 1.26 5.55 1 5 1H1.75z"></path>
-                    </svg>
-                    {folder}
-                </div>
-            );
+        UserService.getProfileImg(username).then((url) => {
+            setProfileImg(url);
         });
-    }
-    function displayItems() {
-        return repoItems.map((item) => {
+    }, [user]);
+    function displayRepoContent(list, folder) {
+        const folderIcon = (
+            <svg
+                fill="#8b949e"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-label="Directory"
+                aria-hidden="true"
+                height="16"
+                viewBox="0 0 16 16"
+                version="1.1"
+                width="16"
+                data-view-component="true"
+            >
+                <path d="M1.75 1A1.75 1.75 0 000 2.75v10.5C0 14.216.784 15 1.75 15h12.5A1.75 1.75 0 0016 13.25v-8.5A1.75 1.75 0 0014.25 3H7.5a.25.25 0 01-.2-.1l-.9-1.2C6.07 1.26 5.55 1 5 1H1.75z"></path>
+            </svg>
+        );
+        const fileIcon = (
+            <svg
+                fill="#8b949e"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-label="File"
+                aria-hidden="true"
+                height="16"
+                viewBox="0 0 16 16"
+                version="1.1"
+                width="16"
+                data-view-component="true"
+            >
+                <path
+                    fill="#8b949e"
+                    fillRule="evenodd"
+                    d="M3.75 1.5a.25.25 0 00-.25.25v12.5c0 .138.112.25.25.25h9.5a.25.25 0 00.25-.25V6h-2.75A1.75 1.75 0 019 4.25V1.5H3.75zm6.75.062V4.25c0 .138.112.25.25.25h2.688a.252.252 0 00-.011-.013l-2.914-2.914a.272.272 0 00-.013-.011zM2 1.75C2 .784 2.784 0 3.75 0h6.586c.464 0 .909.184 1.237.513l2.914 2.914c.329.328.513.773.513 1.237v9.586A1.75 1.75 0 0113.25 16h-9.5A1.75 1.75 0 012 14.25V1.75z"
+                ></path>
+            </svg>
+        );
+        const icon = folder ? folderIcon : fileIcon;
+        return list.map((item) => {
             return (
                 <div key={item} className="view-repo-item">
-                    <svg
-                        fill="#8b949e"
-                        xmlns="http://www.w3.org/2000/svg"
-                        aria-label="File"
-                        aria-hidden="true"
-                        height="16"
-                        viewBox="0 0 16 16"
-                        version="1.1"
-                        width="16"
-                        data-view-component="true"
-                        className="margin-right"
+                    <div
+                        style={{
+                            width: "32px",
+                            display: "flex",
+                            justifyContent: "center",
+                        }}
                     >
-                        <path
-                            fill="#8b949e"
-                            fillRule="evenodd"
-                            d="M3.75 1.5a.25.25 0 00-.25.25v12.5c0 .138.112.25.25.25h9.5a.25.25 0 00.25-.25V6h-2.75A1.75 1.75 0 019 4.25V1.5H3.75zm6.75.062V4.25c0 .138.112.25.25.25h2.688a.252.252 0 00-.011-.013l-2.914-2.914a.272.272 0 00-.013-.011zM2 1.75C2 .784 2.784 0 3.75 0h6.586c.464 0 .909.184 1.237.513l2.914 2.914c.329.328.513.773.513 1.237v9.586A1.75 1.75 0 0113.25 16h-9.5A1.75 1.75 0 012 14.25V1.75z"
-                        ></path>
-                    </svg>
+                        {icon}
+                    </div>
                     {item}
                 </div>
             );
@@ -81,7 +91,22 @@ const ViewRepository = () => {
                 <section className="left-repo-column">
                     <div className="row align-center space-between">
                         <div className="vertical-center">
-                            <button className="secondary-gray-btn btn">
+                            <button className="secondary-gray-btn btn vertical-center">
+                                <svg
+                                    fill="#8b949e"
+                                    text="gray"
+                                    aria-hidden="true"
+                                    height="16"
+                                    viewBox="0 0 16 16"
+                                    version="1.1"
+                                    width="16"
+                                    data-view-component="true"
+                                >
+                                    <path
+                                        fillRule="evenodd"
+                                        d="M11.75 2.5a.75.75 0 100 1.5.75.75 0 000-1.5zm-2.25.75a2.25 2.25 0 113 2.122V6A2.5 2.5 0 0110 8.5H6a1 1 0 00-1 1v1.128a2.251 2.251 0 11-1.5 0V5.372a2.25 2.25 0 111.5 0v1.836A2.492 2.492 0 016 7h4a1 1 0 001-1v-.628A2.25 2.25 0 019.5 3.25zM4.25 12a.75.75 0 100 1.5.75.75 0 000-1.5zM3.5 3.25a.75.75 0 111.5 0 .75.75 0 01-1.5 0z"
+                                    ></path>
+                                </svg>
                                 main ▾
                             </button>
                             <p>2 branches</p>
@@ -111,16 +136,24 @@ const ViewRepository = () => {
                     <div className="view-repo-content">
                         <div
                             className="view-repo-item border-divider"
-                            style={{ backgroundColor: "rgb(22,27,34)" }}
+                            style={{
+                                backgroundColor: "rgb(22,27,34)",
+                                gap: ".5em",
+                            }}
                         >
+                            <img
+                                src={profileImg}
+                                alt="Profile icon"
+                                className="profile-icon round-profile-img"
+                            />
                             {username}
                         </div>
-                        {displayFolders()}
-                        {displayItems()}
+                        {displayRepoContent(repoFolders, true)}
+                        {displayRepoContent(repoItems, false)}
                     </div>
                 </section>
                 <div className="view-repo-side-content right-repo-column">
-                    <div>
+                    <div className="border-divider-dark">
                         <h4>About</h4>
                         <p className="about-entry vertical-center">
                             <svg
@@ -131,7 +164,6 @@ const ViewRepository = () => {
                                 version="1.1"
                                 width="16"
                                 data-view-component="true"
-                                className="margin-right"
                             >
                                 <path
                                     fillRule="evenodd"
@@ -149,7 +181,6 @@ const ViewRepository = () => {
                                 version="1.1"
                                 width="16"
                                 data-view-component="true"
-                                className="margin-right"
                             >
                                 <path
                                     fillRule="evenodd"
@@ -167,14 +198,13 @@ const ViewRepository = () => {
                                 version="1.1"
                                 width="16"
                                 data-view-component="true"
-                                className="margin-right"
                             >
                                 <path
                                     fillRule="evenodd"
                                     d="M1.679 7.932c.412-.621 1.242-1.75 2.366-2.717C5.175 4.242 6.527 3.5 8 3.5c1.473 0 2.824.742 3.955 1.715 1.124.967 1.954 2.096 2.366 2.717a.119.119 0 010 .136c-.412.621-1.242 1.75-2.366 2.717C10.825 11.758 9.473 12.5 8 12.5c-1.473 0-2.824-.742-3.955-1.715C2.92 9.818 2.09 8.69 1.679 8.068a.119.119 0 010-.136zM8 2c-1.981 0-3.67.992-4.933 2.078C1.797 5.169.88 6.423.43 7.1a1.619 1.619 0 000 1.798c.45.678 1.367 1.932 2.637 3.024C4.329 13.008 6.019 14 8 14c1.981 0 3.67-.992 4.933-2.078 1.27-1.091 2.187-2.345 2.637-3.023a1.619 1.619 0 000-1.798c-.45-.678-1.367-1.932-2.637-3.023C11.671 2.992 9.981 2 8 2zm0 8a2 2 0 100-4 2 2 0 000 4z"
                                 ></path>
                             </svg>{" "}
-                            1 watching
+                            0 watching
                         </p>
                         <p className="about-entry vertical-center">
                             <svg
@@ -185,7 +215,6 @@ const ViewRepository = () => {
                                 version="1.1"
                                 width="16"
                                 data-view-component="true"
-                                className="margin-right"
                             >
                                 <path
                                     fillRule="evenodd"
@@ -195,14 +224,27 @@ const ViewRepository = () => {
                             0 Forks
                         </p>
                     </div>
-                    <div>
+                    <div className="border-divider-dark">
                         <h4>Releases</h4>
+                        <small className="secondary-text">
+                            No releases published <br />
+                        </small>
+                        <small>
+                            <a href="#" className="blue-accent-text">
+                                Create a new release
+                            </a>
+                        </small>
                     </div>
-                    <div>
+                    <div className="border-divider-dark">
                         <h4>Packages</h4>
-                    </div>
-                    <div>
-                        <h4>Environments</h4>
+                        <small className="secondary-text">
+                            No packages published <br />
+                        </small>
+                        <small>
+                            <a href="#" className="blue-accent-text">
+                                Publish your first package
+                            </a>
+                        </small>
                     </div>
                 </div>
             </div>
