380a56927875ecb7919149bd5109cafa2644a11d
BrandonC123 <brandoc318@gmail.com>
2022-10-26 22:34:11 -0700

    Test editing profile and when to display follow button.

--- src/__tests__/ViewProfile.test.js
+++ src/__tests__/ViewProfile.test.js
@@ -1,4 +1,5 @@
 import { cleanup, render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { MemoryRouter, useParams } from "react-router-dom";
 import { UserContext } from "../components/UserContext";
 import ViewProfile from "../components/ViewProfile";
@@ -12,6 +13,7 @@ const mockUser = {
 
 const emptyUser = {
     profileImgSrc: "../img/404.png",
+    name: "Brandon Chu",
     username: "Brandon",
     bio: "my name is brandon",
     website: "google.com",
@@ -23,6 +25,7 @@ const emptyUser = {
             starCount: 0,
         },
     ],
+    followList: [],
 };
 
 jest.mock("../services//RepositoryService.jsx");
@@ -89,4 +92,54 @@ describe("View profile route", () => {
         );
         expect(await screen.findByText("Battleship")).toBeInTheDocument();
     });
+    test("Edit profile form displays", async function () {
+        intializeProfileMocks();
+        render(
+            <UserContext.Provider value={mockUser}>
+                <ViewProfile />
+            </UserContext.Provider>,
+            { wrapper: MemoryRouter }
+        );
+        userEvent.click(await screen.findByText("Edit Profile"));
+        expect(await screen.findByLabelText("Name")).toBeVisible();
+        expect(await screen.findByLabelText("Bio")).toBeVisible();
+    });
+    test("Profile changes are displayed", async function () {
+        intializeProfileMocks();
+        render(
+            <UserContext.Provider value={mockUser}>
+                <ViewProfile />
+            </UserContext.Provider>,
+            { wrapper: MemoryRouter }
+        );
+        userEvent.click(await screen.findByText("Edit Profile"));
+
+        const nameInput = await screen.findByLabelText("Name");
+        userEvent.type(nameInput, "John Doe");
+
+        expect(screen.getByText("Brandon Chu")).toBeInTheDocument();
+
+        const saveBtn = await screen.findByText("Save");
+        userEvent.click(saveBtn);
+
+        expect(screen.queryByText("Brandon Chu")).not.toBeInTheDocument();
+        expect(screen.getByText("John Doe")).toBeInTheDocument();
+    });
+    test("Follow button is displayed when not on own account", async function () {
+        intializeProfileMocks();
+        render(
+            <UserContext.Provider
+                value={{
+                    displayName: "johnSmith",
+                    photoURL: "../img/404.png",
+                }}
+            >
+                <ViewProfile />
+            </UserContext.Provider>,
+            { wrapper: MemoryRouter }
+        );
+        const followBtn = await screen.findByText("Follow");
+        expect(followBtn).toBeInTheDocument();
+    });
+    
 });
